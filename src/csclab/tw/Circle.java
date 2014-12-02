59efe66966e4b23752148cd13652efc827f61f52
oberon <st900278@gmail.com>
2014-12-02 16:00:26 +0800

    finish Circle Class

--- src/csclab/tw/Circle.java
+++ src/csclab/tw/Circle.java
@@ -3,15 +3,13 @@ package csclab.tw;
 public class Circle extends Shape {
 	
 	private double radius;
-	
 	public Circle(double radius){
 		this.radius = radius;
 	}
 	
 	@Override
 	public double getArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 3.14*radius*radius;
 	}
 
 }
